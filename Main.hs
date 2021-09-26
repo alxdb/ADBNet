7c27202dd34cccff87c5f23dfd783e926f10f29d
Alexander Davidson Bryan <alxdb@pm.me>
2021-09-27 00:27:22 +0100

    Switch back to HMatrix

--- Main.hs
+++ Main.hs
@@ -2,23 +2,20 @@
 
 module Main where
 
+import Control.Monad
 import Data.Function
 import qualified Data.Vector as V
 import Debug.Trace
-import Linear hiding (trace)
+import Numeric.LinearAlgebra
 import System.Random.Stateful
 
-type Vector a = V.Vector a
+randMat :: (StatefulGen g m) => (Int, Int) -> g -> m (Matrix R)
+randMat (rows, cols) g = (rows >< cols) <$> replicateM (rows * cols) (uniformRM (0.0, 1.0) g)
 
-type Matrix a = Vector (Vector a)
-
-randMat :: (UniformRange a, Fractional a, StatefulGen g m) => (Int, Int) -> g -> m (Matrix a)
-randMat (rows, cols) g = V.replicateM rows . V.replicateM cols $ uniformRM (0.0, 1.0) g
-
-type Network = Vector (Matrix Double)
+type Network = V.Vector (Matrix R)
 
 randNet :: (StatefulGen g m) => [Int] -> g -> m Network
-randNet dims g = V.fromList <$> mapM (`randMat` g) (zipWith (\i o -> (o, i)) dims (tail dims))
+randNet dims g = V.fromList <$> mapM (`randMat` g) (zipWith (\i o -> (o, i + 1)) dims (tail dims))
 
 sigmoid :: Floating a => a -> a
 sigmoid x = 1 / (1 + exp x)
@@ -35,38 +32,42 @@ rss' x y = 2 * (x - y)
 memoize :: Int -> (Int -> a) -> (Int -> a)
 memoize l f = (V.generate l f V.!)
 
-type Activations = Vector (Vector Double, Vector Double)
+type Activations = V.Vector (Vector R, Vector R)
 
-activations :: Network -> Vector Double -> Activations
+activations :: Network -> Vector R -> Activations
 activations net input = V.generate (length net) (activation net input)
   where
+    activation :: Network -> Vector R -> Int -> (Vector R, Vector R)
     activation net input = fix (memoize (length net) . f)
       where
-        f _ 0 = trace "activate 0" $ let z = V.head net !* input in (V.map sigmoid z, z)
-        f f' n = trace ("activate " ++ show n) $ let z = net V.! n !* fst (f' (n - 1)) in (V.map sigmoid z, z)
+        f :: (Int -> (Vector R, Vector R)) -> Int -> (Vector R, Vector R)
+        f _ 0 = trace "activate 0" $ let z = V.head net #> vjoin [input, 1.0] in (sigmoid z, z)
+        f f' n = trace ("activate " ++ show n) $ let z = net V.! n #> vjoin [fst (f' (n - 1)), 1.0] in (sigmoid z, z)
 
-deltas :: Network -> Vector Double -> Vector Double -> Activations -> Vector (Vector Double)
+deltas :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R)
 deltas net input output as = V.generate (length net) (delta net input output as)
   where
+    delta :: Network -> Vector R -> Vector R -> Activations -> Int -> Vector R
     delta net input output as = fix (memoize (length net) . f)
       where
+        f :: (Int -> Vector R) -> Int -> Vector R
         f f' n
-          | n == length net - 1 = trace ("delta " ++ show n) $ V.zipWith (*) z' (V.zipWith rss' a output)
-          | otherwise = trace ("delta " ++ show n) $ V.zipWith (*) z' (c !* f' (n + 1))
+          | n == length net - 1 = trace ("delta " ++ show n) $ z' * rss' a output
+          | otherwise = trace ("delta " ++ show n) $ z' * (tr w #> f' (n + 1))
           where
             (a, z) = as V.! n
-            c = net V.! n
-            z' = V.map sigmoid' z
+            w = net V.! n ¿ [0 .. size a - 2]
+            z' = sigmoid' z
 
-gradients :: Network -> Vector Double -> Vector Double -> Activations -> Vector (Vector Double) -> Vector (Matrix Double)
+gradients :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R) -> V.Vector (Matrix R)
 gradients net input output as ds = V.generate (length net) (gradient net input output as ds)
   where
+    gradient :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R) -> Int -> Matrix R
     gradient net input output as ds = fix (memoize (length net) . f)
       where
-        f f' 0 = trace "gradient 0" $ outer input (V.head ds)
-        f f' n = trace ("gradient " ++ show n) $ outer (fst $ as V.! (n - 1)) (ds V.! n)
+        f f' 0 = trace "gradient 0" $ input `outer` V.head ds
+        f f' n = trace ("gradient " ++ show n) $ let (a, _) = as V.! (n - 1) in a `outer` (ds V.! n)
 
-main :: IO ()
 main = do
   gen <- newIOGenM $ mkStdGen 10
   net <- randNet [2, 4, 4, 1] gen
