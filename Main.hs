ecee9d54d2554176e41afd9b7d0a837aa5fdba1f
Alexander Davidson Bryan <alxdb@pm.me>
2021-09-27 21:25:22 +0100

    Simplified Further

--- Main.hs
+++ Main.hs
@@ -5,9 +5,13 @@ module Main where
 import Control.Monad
 import Data.Function
 import qualified Data.Vector as V
-import Debug.Trace
 import Numeric.LinearAlgebra
 import System.Random.Stateful
+import Text.Printf
+
+-- | To be replaced with Debug.Trace when debugging
+trace :: String -> b -> b
+trace _ x = x
 
 randMat :: (StatefulGen g m) => (Int, Int) -> g -> m (Matrix R)
 randMat (rows, cols) g = (rows >< cols) <$> replicateM (rows * cols) (uniformRM (0.0, 1.0) g)
@@ -29,44 +33,30 @@ rss x y = (x - y) ** 2
 rss' :: Floating a => a -> a -> a
 rss' x y = 2 * (x - y)
 
-memoize :: Int -> (Int -> a) -> (Int -> a)
-memoize l f = (V.generate l f V.!)
-
 type Activations = V.Vector (Vector R, Vector R)
 
 activations :: Network -> Vector R -> Activations
-activations net input = V.generate (length net) (activation net input)
+activations net input = result
   where
-    activation :: Network -> Vector R -> Int -> (Vector R, Vector R)
-    activation net input = fix (memoize (length net) . f)
-      where
-        f :: (Int -> (Vector R, Vector R)) -> Int -> (Vector R, Vector R)
-        f _ 0 = trace "activate 0" $ let z = V.head net #> vjoin [input, 1.0] in (sigmoid z, z)
-        f f' n = trace ("activate " ++ show n) $ let z = net V.! n #> vjoin [fst (f' (n - 1)), 1.0] in (sigmoid z, z)
+    result = V.generate (length net) f
+    f 0 = trace "a 0" $ let z = V.head net #> vjoin [input, 1.0] in (sigmoid z, z)
+    f n = trace (printf "a %d" n) $ let z = net V.! n #> vjoin [fst (result V.! (n - 1)), 1.0] in (sigmoid z, z)
 
 deltas :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R)
-deltas net input output as = V.generate (length net) (delta net input output as)
+deltas net input output as = result
   where
-    delta :: Network -> Vector R -> Vector R -> Activations -> Int -> Vector R
-    delta net input output as = fix (memoize (length net) . f)
+    result = V.generate (length net) f
+    f n = trace (printf "d %d" n) $ z' * if n == length net - 1 then rss' a output else tr w #> result V.! (n + 1)
       where
-        f :: (Int -> Vector R) -> Int -> Vector R
-        f f' n
-          | n == length net - 1 = trace ("delta " ++ show n) $ z' * rss' a output
-          | otherwise = trace ("delta " ++ show n) $ z' * (tr w #> f' (n + 1))
-          where
-            (a, z) = as V.! n
-            w = (net V.! (n + 1)) ¿ [0 .. size a - 1]
-            z' = sigmoid' z
+        (a, z) = as V.! n
+        w = takeColumns (size a) (net V.! (n + 1))
+        z' = sigmoid' z
 
 gradients :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R) -> V.Vector (Matrix R)
-gradients net input output as ds = V.generate (length net) (gradient net input output as ds)
+gradients net input output as ds = V.generate (length net) f
   where
-    gradient :: Network -> Vector R -> Vector R -> Activations -> V.Vector (Vector R) -> Int -> Matrix R
-    gradient net input output as ds = fix (memoize (length net) . f)
-      where
-        f f' 0 = trace "gradient 0" $ input `outer` V.head ds
-        f f' n = trace ("gradient " ++ show n) $ let (a, _) = as V.! (n - 1) in a `outer` (ds V.! n)
+    f 0 = trace "g 0" $ V.head ds `outer` vjoin [input, 1.0]
+    f n = trace (printf "g %d" n) $ (ds V.! n) `outer` vjoin [fst (as V.! (n - 1)), 1.0]
 
 main = do
   gen <- newIOGenM $ mkStdGen 10
