76de860251f95994427281f4e6e97808063dbf67
Alexander Davidson Bryan <alxdb@pm.me>
2021-09-30 00:22:30 +0100

    More Tensors

--- src/ADBNet/Tensor.hs
+++ src/ADBNet/Tensor.hs
@@ -23,8 +23,8 @@ instance Show Vector where
 
 class Ix a => Tix a where
   start :: a
-  trange :: a -> (a, a)
-  trange s = (start, s)
+  tRange :: a -> (a, a)
+  tRange s = (start, s)
 
 instance Tix (Int, Int) where
   start = (1, 1)
@@ -32,76 +32,18 @@ instance Tix (Int, Int) where
 instance Tix Int where
   start = 1
 
-tmap :: (Ix a) => (R -> R) -> Tensor a -> Tensor a
-tmap f = Tensor . amap f . arr
+tMap :: (Ix a) => (R -> R) -> Tensor a -> Tensor a
+tMap f = Tensor . amap f . arr
 
-tdim :: (Ix a) => Tensor a -> a
-tdim = snd . bounds . arr
+tDim :: (Ix a) => Tensor a -> a
+tDim = snd . bounds . arr
 
-tnew :: (Tix a) => a -> [R] -> Tensor a
-tnew s v = Tensor $ listArray (trange s) v
+tNew :: (Tix a) => a -> [R] -> Tensor a
+tNew s v = Tensor $ listArray (tRange s) v
 
-trnd :: (Tix a, StatefulGen g m) => a -> g -> m (Tensor a)
-trnd s g =
-  tnew s <$> replicateM (rangeSize . trange $ s) (uniformRM (0.0, 1.0) g)
+tRndM :: (Tix a, StatefulGen g m) => a -> g -> m (Tensor a)
+tRndM s g =
+  tNew s <$> replicateM (rangeSize . tRange $ s) (uniformRM (0.0, 1.0) g)
 
--- class ITensor t a where
---   tnew :: a -> [R] -> t a
-
--- instance ITensor Tensor (Int, Int) where
---   tnew s v = Tensor $ listArray ((1, 1), s) v
-
--- instance ITensor Tensor Int where
---   tnew s v = Tensor $ listArray (1, s) v
-
--- mrnd :: StatefulGen g m => (Int, Int) -> g -> m Matrix
--- mrnd s@(r, c) g = tnew . array
-
--- type family Rank t
--- type instance Rank Matrix = (Int, Int)
--- type instance Rank Vector = Int
-
--- newtype Matrix = Matrix (UArray (Rank Matrix) R)
--- newtype Vector = Vector (UArray (Rank Vector) R)
-
-
--- class Tensor t where
---   toArray :: t -> UArray (Rank t) R
---   fromArray :: UArray (Rank t) R -> t
-
--- instance Tensor Matrix where
---   toArray (Matrix arr) = arr
---   fromArray arr = Matrix arr
-
--- instance Tensor Vector where
---   toArray (Vector arr) = arr
---   fromArray arr = Vector arr
-
-  -- dim :: t -> Rank t
-  -- dim = snd . bounds . toArray
-
-  -- new :: Rank t -> [R] -> t
-  -- rnd :: StatefulGen g m => Rank t -> g -> m t
-
-  -- map f (Matrix arr) = Matrix $ amap f arr
-  -- dim (Matrix arr) = let (x, y) = fst . bounds $ arr in (x + 1, y + 1)
-  -- new size@(rows, cols) v = Matrix . array b $ zip i v
-  --  where
-  --   r = rows - 1
-  --   c = cols - 1
-  --   b = ((0, 0), (r, c))
-  --   i = [ (i, j) | i <- [0 .. r], j <- [0 .. c] ]
-  -- rnd size@(rows, cols) g = do
-  --   values <- replicateM (rows * cols) $ uniformRM (0.0, 1.0) g
-  --   return $ new size values
-
-  -- map f (Vector arr) = Vector $ amap f arr
-  -- dim (Vector arr) = let x = fst . bounds $ arr in x + 1
-  -- new length v = Vector . array b $ zip i v
-  --  where
-  --   l = length - 1
-  --   b = (0, l)
-  --   i = [0 .. l]
-  -- rnd length v = do
-  --   values <- replicateM length $ uniformRM (0.0, 1.0) g
-  --   return $ new length values
+tRnd :: (Tix a, RandomGen g) => a -> g -> (Tensor a, g)
+tRnd s g = runStateGen g (tRndM s)
